b8b215589d273de49ef8211e622a453c26b871fa
Alex Boster <alex@foodydirect.com>
2013-08-18 14:01:40 -0700

    ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Recallfinder::Application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :signature_collection_locations
   resources :location_searches
 
@@ -59,4 +61,7 @@ Recallfinder::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  ActiveAdmin.routes(self)
+
 end
