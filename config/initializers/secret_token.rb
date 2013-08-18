6f73acef8250752b8a2a7021bf29d661bcf5f990
Alex Boster <alex@foodydirect.com>
2013-08-18 08:00:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recallfinder::Application.config.secret_token = '7c27b6acf93e77b86b942881ea8d988f147da888f0d75d1243068d13a0970a89978d0020d70ebb08fb6e8c91913169f8eefccd31f688acbff47149f19ad14ffb'
